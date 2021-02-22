2d91b8118c2770ed59d5374f59984cd8b7c756d5
Bhagavan Swaroop Pentapati <bhagavan.pentapati@gmail.com>
2021-02-22 18:32:04 +0530

    Update NewBranchHelloWorld.java

--- NewBranchHelloWorld.java
+++ NewBranchHelloWorld.java
@@ -3,5 +3,6 @@ package com.test;
 class NewBranchHelloWorld {
   public static void main(String[] args) {
     System.out.println("Welcome to New Branch!");
+    System.out.println("Added new line");
   }
 }
